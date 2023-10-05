c801d2c0e5c604313163f5d5a44711cbfbdcb997
Duc <luuduc34@hotmail.com>
2023-10-05 08:49:21 +0200

    add gradle implementations & plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,22 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    //nav
+    id("androidx.navigation.safeargs.kotlin")
+
+    //room
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.technifutur.andeval5"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.technifutur.andeval5"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,6 +39,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -43,4 +52,24 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //nav
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.3")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.3")
+
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
+
+    //picasso
+    implementation ("com.squareup.picasso:picasso:2.8")
+
+    //room
+    implementation("androidx.room:room-runtime:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+
+    //bottom menu
+    implementation ("com.google.android.material:material:1.2.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+//nav
+buildscript{
+    repositories{
+        google()
+    }
+    dependencies{
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.0")
+    }
+}
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+
+    //room
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }

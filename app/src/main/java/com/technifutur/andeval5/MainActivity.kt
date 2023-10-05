f594db2dd2515894d714c8e8a455af39f32da03d
Duc <luuduc34@hotmail.com>
2023-10-05 09:04:14 +0200

    add bottom menu and link navgraph and menu to main activity

--- app/src/main/java/com/technifutur/andeval5/MainActivity.kt
+++ app/src/main/java/com/technifutur/andeval5/MainActivity.kt
@@ -2,6 +2,8 @@ package com.technifutur.andeval5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.technifutur.andeval5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val bottomNavigationView = binding.bottomNavigationView
+        val navController = findNavController(R.id.navHostFragment)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
